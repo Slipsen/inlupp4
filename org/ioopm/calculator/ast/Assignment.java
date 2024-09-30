1d77c8e6509d55947947d5b8fe8eb45c5465c00b
Jakob Hjelm Redin <jakob.hjelm.redin@gmail.com>
2024-09-30 16:16:47 +0200

    minor

--- org/ioopm/calculator/ast/Assignment.java
+++ org/ioopm/calculator/ast/Assignment.java
@@ -1,7 +1,11 @@
 package org.ioopm.calculator.ast;
 
 public class Assignment extends Binary {
-
+    /**binary tree node for storving both tes of ann assignment
+     * 
+     * @param lhs to be hold in elft branch
+     * @param rhs to be held in right branch
+     */
     public Assignment(SymbolicExpression lhs, SymbolicExpression rhs) {
         super(lhs, rhs, 400,"=");
     }
--- org/ioopm/calculator/ast/Atom.java
+++ org/ioopm/calculator/ast/Atom.java
@@ -3,7 +3,7 @@ package org.ioopm.calculator.ast;
 public abstract class Atom extends SymbolicExpression {
 
 
-    
+    /**atoms can't have anyvariable unless a variable */
     boolean varExists(Environment vars, SymbolicExpression sy) {
         return false; 
     }
